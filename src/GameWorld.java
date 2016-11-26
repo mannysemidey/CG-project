27577ff3ab88f90f792fc2cfea68a736d73e46a5
Manny Semidey <mannysemidey@gmail.com>
2016-11-26 12:02:08 -0600

    Revert "Merge remote-tracking branch 'kds130130/master'"
    This reverts commit 98ea626acb76fde2fde5c355f21970a74b46d589, reversing
    changes made to 40585b7f45bf8a0155615015d09588e5a1c448f5.

--- src/GameWorld.java
+++ src/GameWorld.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 //TODO: Add more pixelTypes. Update ELEMENT_COUNT
 
 //Class responsible for managing game world objects
@@ -10,10 +8,7 @@ public class GameWorld
     //2Darray indices represent (x,y) coordinates of each pixels
     final int PIXEL_MAP_HEIGHT = 500;
     final int PIXEL_MAP_WIDTH = 500;
-    Particle[][] pixelMap;//(0,0) coordinates are located at the top-left of the 2D array
-    
-    //General-use random variable
-    Random randy = new Random();
+    Particle[][] pixelMap;
     
     //Associates an element name with an array index.
     //Determines name of particle on pixelMap where
@@ -21,21 +16,18 @@ public class GameWorld
     final int ELEMENT_COUNT = 3;
     String[] pixelNameList = new String[ELEMENT_COUNT];
     
-    //Boolean variables to keep track of mouse operations by user
-    boolean mouseWasClicked;//Turns "true" when the visual component indicates the mouse was clicked
-    int clickedX, clickedY;//Stores x,y coordinates of the last point the mouse was clicked
-    boolean mouseWasDragged;//Turns "true" when the visual component indicates the mouse was dragged
-    int draggedX1, draggedX2, draggedY1, draggedY2;//x,y coordinates of dragging mouse's the starting point and ending point
     
-    //Booleans to keep track of toggled options
-    boolean wallCollision;
+    //Boolean variables to keep track of mouse operations by user
+    boolean mouseIsDown;
+    boolean mouseIsUp;
+    boolean mouseClicked;
     
     //Default constructor
     public GameWorld()
     {
         //Initializes pixelMap with "Nothing" in all indices
         pixelMap = new Particle[PIXEL_MAP_WIDTH][PIXEL_MAP_HEIGHT];
-        for(int iY = PIXEL_MAP_HEIGHT; iY > 0; iY--){
+        for(int iY = 0; iY < PIXEL_MAP_HEIGHT; iY++){
             for(int iX = 0; iX < PIXEL_MAP_WIDTH; iX++){
                 pixelMap[iX][iY] = new Particle("Nothing", "Black", true, iX, iY);
             }
@@ -43,160 +35,15 @@ public class GameWorld
         
         //Initializes pixelType with every element to be used in-game
         pixelNameList[0] = "Nothing";
-        pixelNameList[1] = "Sand";//Completely unreactive non-static particle
-        pixelNameList[2] = "Wall";//Completely unreactive static paticle
+        pixelNameList[1] = "Sand";
+        pixelNameList[2] = "Wall";
         
-        //Initializes mouse variables
-        mouseWasClicked = false;
-        mouseWasDragged = false;
-        
-        //Initializes toggled option variables
-        wallCollision = false;
     }
     
     //Assesses the situation and determines the next course of action for the game
     //We must find a way to call this repeatedly during gamepay
     public void update(){
         
-        //Updates each pixel on-screen, bottom to top, in horizontal layers
-        for(int iY = PIXEL_MAP_HEIGHT; iY > 0; iY--){
-            for(int iX = 0; iX < PIXEL_MAP_WIDTH; iX++){
-                
-                updatePixel(pixelMap[iX][iY]);
-                
-                //Removes any pixels at the screen edge if wall-collision is off
-                if(!wallCollision && (iX == 0 || iY == 0 || iX == PIXEL_MAP_WIDTH-1 || iY == PIXEL_MAP_HEIGHT-1))
-                {
-                    pixelMap[iX][iY] = new Particle(iX,iY);
-                }
-            }
-        }
-        
-    }
-    
-    //Updates the pixel space with the given particle
-    //only considers pixel spaces below the given particle
-    public void updatePixel(Particle thisPixel){
-        //Change nothing if there's nothing in the pixel space
-        if(thisPixel.name == "Nothing")
-        {
-            return;
-        } 
-        //Change nothing if particle doesn't fall
-        else if(thisPixel.isStatic)
-        {
-            return;
-        }
-        //Moves particle 1 pixel downward if there's nothing in the pixel space below and the particle is not at the bottom of the screen
-        else if(pixelMap[thisPixel.x][thisPixel.y+1].name == "Nothing" && thisPixel.y+1 < PIXEL_MAP_HEIGHT-1){
-            pixelMap[thisPixel.x][thisPixel.y+1] = pixelMap[thisPixel.x][thisPixel.y];
-            pixelMap[thisPixel.x][thisPixel.y] = new Particle(thisPixel.x, thisPixel.y);
-            pixelMap[thisPixel.x][thisPixel.y+1].y++;
-        }
-        else{
-            //Checks if the particle below can interect with thisParticle
-            if(canParticleInteract(pixelMap[thisPixel.x][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]))
-            {
-                particleInteract(pixelMap[thisPixel.x][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]);
-            }
-            //Since there's an unreactive particle directly below, thisPixel randomly decides to try to slide left or slide right
-            else if(randy.nextInt(2) == 0)
-            {//Checks right side first
-                if(pixelMap[thisPixel.x+1][thisPixel.y+1].name == "Nothing" && thisPixel.y+1 < PIXEL_MAP_HEIGHT-1 && thisPixel.x+1 < PIXEL_MAP_WIDTH-1)
-                {
-                    pixelMap[thisPixel.x+1][thisPixel.y+1] = pixelMap[thisPixel.x][thisPixel.y];
-                    pixelMap[thisPixel.x][thisPixel.y] = new Particle(thisPixel.x, thisPixel.y);
-                    pixelMap[thisPixel.x+1][thisPixel.y+1].x++;
-                    pixelMap[thisPixel.x+1][thisPixel.y+1].y++;
-                }
-                //Checks if the particle below+right can interect with thisParticle
-                else if(canParticleInteract(pixelMap[thisPixel.x+1][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]))
-                {
-                    particleInteract(pixelMap[thisPixel.x+1][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]);
-                }
-                //Checking left side
-                else if(pixelMap[thisPixel.x-1][thisPixel.y+1].name == "Nothing" && thisPixel.y+1 < PIXEL_MAP_HEIGHT-1 && thisPixel.x-1 > 0)
-                {
-                    pixelMap[thisPixel.x-1][thisPixel.y+1] = pixelMap[thisPixel.x][thisPixel.y];
-                    pixelMap[thisPixel.x][thisPixel.y] = new Particle(thisPixel.x, thisPixel.y);
-                    pixelMap[thisPixel.x-1][thisPixel.y+1].y++;
-                    pixelMap[thisPixel.x-1][thisPixel.y+1].x--;
-                }
-                //Checks if the particle below+left can interect with thisParticle
-                else if(canParticleInteract(pixelMap[thisPixel.x-1][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]))
-                {
-                    particleInteract(pixelMap[thisPixel.x-1][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]);
-                }
-            }
-            else
-            {//Checks left side first
-                if(pixelMap[thisPixel.x-1][thisPixel.y+1].name == "Nothing" && thisPixel.y+1 < PIXEL_MAP_HEIGHT-1 && thisPixel.x-1 > 0)
-                {
-                    pixelMap[thisPixel.x-1][thisPixel.y+1] = pixelMap[thisPixel.x][thisPixel.y];
-                    pixelMap[thisPixel.x][thisPixel.y] = new Particle(thisPixel.x, thisPixel.y);
-                    pixelMap[thisPixel.x-1][thisPixel.y+1].y++;
-                    pixelMap[thisPixel.x-1][thisPixel.y+1].x--;
-                }
-                //Checks if the particle below+left can interect with thisParticle
-                else if(canParticleInteract(pixelMap[thisPixel.x-1][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]))
-                {
-                    particleInteract(pixelMap[thisPixel.x-1][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]);
-                }
-                //Checking right side
-                else if(pixelMap[thisPixel.x+1][thisPixel.y+1].name == "Nothing" && thisPixel.y+1 < PIXEL_MAP_HEIGHT-1 && thisPixel.x+1 < PIXEL_MAP_WIDTH-1)
-                {
-                    pixelMap[thisPixel.x+1][thisPixel.y+1] = pixelMap[thisPixel.x][thisPixel.y];
-                    pixelMap[thisPixel.x][thisPixel.y] = new Particle(thisPixel.x, thisPixel.y);
-                    pixelMap[thisPixel.x+1][thisPixel.y+1].x++;
-                    pixelMap[thisPixel.x+1][thisPixel.y+1].y++;
-                }
-                //Checks if the particle below+right can interect with thisParticle
-                else if(canParticleInteract(pixelMap[thisPixel.x+1][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]))
-                {
-                    particleInteract(pixelMap[thisPixel.x+1][thisPixel.y+1], pixelMap[thisPixel.x][thisPixel.y]);
-                }
-            }
-            
-            //If there's no empty space and no reactive elements underneath, no changes should be made to the particle in-question
-        }
-        
-        
-        
-    }
-    
-    //Updates pixelMap after particleA and particleB interact with each other
-    public void particleInteract(Particle particleA, Particle particleB)
-    {
-        
-    }
-    
-    //Returns true if particleA and particleB can interact with each other
-    public boolean canParticleInteract(Particle particleA, Particle particleB)
-    {
-        /*if((particleA.name == "1" && particleB.name == "2") || (particleA.name == "2" && particleB.name == "1"))
-        {
-            return true;
-        }
-        */
-        return false;
-    }
-    
-    //Called by Display when the mouse is clicked at a specific location on the pixel map
-    //parameters accept the pixelMap coordinate at which the mouse was clicked
-    public void mouseClicked(int x, int y){
-        clickedX = x;
-        clickedY = y;
-        mouseWasClicked = true;
-    }
-    
-    //Called by Display when the mouse was dragged at a specific location on the pixel map
-    //parameters accept the pixelMap coordinate at which the mouse started dragging and stopped dragging
-    public void mouseDragged(int x1, int y1, int x2, int y2){
-        draggedX1 = x1;
-        draggedY1 = y1;
-        draggedX2 = x2;
-        draggedY2 = y2;
-        mouseWasDragged = true;
     }
       
 }
--- src/Particle.java
+++ src/Particle.java
@@ -1,53 +1,13 @@
-//Particle Class
-//Used as a structure object to store a sets of data for each in-game particle
-public class Particle 
-{
-    public String name;
-    public String color;
-    
-    //Pixel map coordinates
-    public int x;
-    public int y;
-    
-    //Determines whether the particle falls due to gravity (not static)
-    //or whether the particle stays still (static)
-    public boolean isStatic;
-    
-    public Particle()
-    {
-        name = "Nothing";
-        color = "Black";
-        x = 0;
-        y = 0;
-        isStatic = false;
-    }
-    
-    public Particle(int xPos, int yPos)
-    {
-        name = "Nothing";
-        color = "Black";
-        x = xPos;
-        y = yPos;
-        isStatic = false;
-    }
-    
-    public Particle(String particleName, String particleColor, boolean isItStatic)
-    {
-        name = particleName;
-        color = particleColor;
-        x = 0;
-        y = 0;
-        isStatic = isItStatic;
-    }
-    
-    public Particle(String particleName, String particleColor, boolean isItStatic, int xPos, int yPos)
-    {
-        name = particleName;
-        color = particleColor;
-        x = xPos;
-        y = yPos;
-        isStatic = isItStatic;
-    }
-    
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author K
+ */
+public class Particle {
     
 }
